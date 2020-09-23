51df309bb6709704c771fabf82b63b25c5f905e0
Teresa Heidt <teresaheidt@hotmail.com>
2020-09-23 16:35:59 -0500

    added readme

--- db/seed.sql
+++ db/seed.sql
@@ -1,34 +1,34 @@
 
 -- insert role;
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("Director", 90000);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Director", 90000, 2);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("CEO", 140000);
+INSERT INTO role (title, salary, department_id)
+VALUES ("CEO", 140000, 4);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("Vice President", 170000);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Vice President", 170000, 5);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("President", 200000);
+INSERT INTO role (title, salary, department_id)
+VALUES ("President", 200000, 6);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("Cook", 40000);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Cook", 40000, 7);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("Housekeeper", 30000);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Housekeeper", 30000, 7);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("Art Director", 70000);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Art Director", 70000, 8);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("Architect", 85000);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Architect", 85000, 9);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("PR manager", 55000);
+INSERT INTO role (title, salary, department_id)
+VALUES ("PR manager", 55000, 10);
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("Coordinator", 45000);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Coordinator", 45000, 4);
 
 -- insert department;
 INSERT INTO department (department_name)
@@ -62,32 +62,32 @@ INSERT INTO department (department_name)
 VALUES ("IT");
 
 -- insert employee;
-INSERT INTO employee (employee_firstName, employee_lastName)
-VALUES ("Jack", "Smith");
+INSERT INTO employee (employee_firstName, employee_lastName, role_id, manager_id)
+VALUES ("Jack", "Smith", 12, null);
 
-INSERT INTO employee (employee_firstName, employee_lastName)
+INSERT INTO employee (employee_firstName, employee_lastName, role_id, manager_id)
 VALUES ("Peach", "Bananna");
 
-INSERT INTO employee (employee_firstName, employee_lastName)
+INSERT INTO employee (employee_firstName, employee_lastName, role_id, manager_id)
 VALUES ("Cash", "Pickles");
 
-INSERT INTO employee (employee_firstName, employee_lastName)
+INSERT INTO employee (employee_firstName, employee_lastName, role_id, manager_id)
 VALUES ("Jolly", "Ranger");
 
-INSERT INTO employee (employee_firstName, employee_lastName)
+INSERT INTO employee (employee_firstName, employee_lastName, role_id, manager_id)
 VALUES ("George", "Wilson");
 
-INSERT INTO employee (employee_firstName, employee_lastName)
+INSERT INTO employee (employee_firstName, employee_lastName, role_id, manager_id)
 VALUES ("Winston", "Churchill");
 
-INSERT INTO employee (employee_firstName, employee_lastName)
+INSERT INTO employee (employee_firstName, employee_lastName, role_id, manager_id)
 VALUES ("Catherine", "Sienna");
 
-INSERT INTO employee (employee_firstName, employee_lastName)
+INSERT INTO employee (employee_firstName, employee_lastName, role_id, manager_id)
 VALUES ("Elizabeth", "Windsor");
 
-INSERT INTO employee (employee_firstName, employee_lastName)
+INSERT INTO employee (employee_firstName, employee_lastName, role_id, manager_id)
 VALUES ("George", "Shaw");
 
-INSERT INTO employee (employee_firstName, employee_lastName)
+INSERT INTO employee (employee_firstName, employee_lastName, role_id, manager_id)
 VALUES ("Stanley", "Cup");
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Employee Tracker 👾🤖👾
+
+## Table of contents:
+* About
+* How
+* Why
+* Techologues used
+* Imgs
+* Links
+
+## About:
+This is an employee tracker to help the user view and interact with information stored in the database.
+
+## How:
+Using MySQL for the database, the user can find an employee, department, salary and manager and view this data in the command line.
+
+## Why:
+Super effective way to manage emplpoyees, departments and salaries. 
+
+## Technologies used:
+Node modules, Inquirer packages & MySQL
+
+## Imgs:
+
+## Links:
+[Link to Github](https://teresaheidt.github.io/Employee-Tracker/)
+
+[Link to video]( )
